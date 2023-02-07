6dfdb029bf79cee0afa7d72b2107d818749e534a
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-02-07 21:34:18 +0000

    Set exit value of test run

--- src/midje_runner/runner.clj
+++ src/midje_runner/runner.clj
@@ -2,5 +2,5 @@
   (:require [midje.repl :as mr]))
 
 (defn -main [& args]
-  (mr/load-facts :all)
-  (System/exit 0))
+  (let [result (mr/load-facts :all)]
+    (System/exit (if (zero? (:failures result)) 0 1))))
